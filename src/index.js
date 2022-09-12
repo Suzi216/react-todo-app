bf178dbbb25c053c4d63db1c078fb2e6280010f3
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-09-13 01:59:44 +0300

    Adding a ToDo using react

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>
 );
 
